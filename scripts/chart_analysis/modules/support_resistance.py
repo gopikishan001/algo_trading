ad1744eaa3a9dfb70d356bfbde2bdb75d2493989
Gopikishan <gkay140@gmail.com>
2025-09-28 20:18:01 +0530

    init

---
+++ scripts/chart_analysis/modules/support_resistance.py
@@ -0,0 +1,12 @@
+class SupportResistance:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass 
+
+    def get_support(self, stock_id, current_price):
+        pass
+
+    def get_resistant(self, stock_id, current_price):
+        pass
